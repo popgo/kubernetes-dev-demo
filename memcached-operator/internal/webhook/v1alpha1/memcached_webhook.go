926d1b45e660f6f71ad1328dd64daa0f42520828
Jianfeng Xu <unidba@gmail.com>
2025-11-14 07:54:23 +0800

    fix deletion validation issue and upload webhook testing

--- memcached-operator/internal/webhook/v1alpha1/memcached_webhook.go
+++ memcached-operator/internal/webhook/v1alpha1/memcached_webhook.go
@@ -109,7 +109,7 @@ func validateMemcachedSpec(memcached *cachev1alpha1.Memcached) error {
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
-// +kubebuilder:webhook:path=/validate-cache-popgo-domain-v1alpha1-memcached,mutating=false,failurePolicy=fail,sideEffects=None,groups=cache.popgo.domain,resources=memcacheds,verbs=create;update,versions=v1alpha1,name=vmemcached-v1alpha1.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-cache-popgo-domain-v1alpha1-memcached,mutating=false,failurePolicy=fail,sideEffects=None,groups=cache.popgo.domain,resources=memcacheds,verbs=create;update;delete,versions=v1alpha1,name=vmemcached-v1alpha1.kb.io,admissionReviewVersions=v1
 
 // MemcachedCustomValidator struct is responsible for validating the Memcached resource
 // when it is created, updated, or deleted.
